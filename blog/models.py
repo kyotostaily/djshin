9b1caf429cb1500feb2c1b89e64509e8ff81ad6c
dongjun <kyotostaily@naver.com>
2022-07-20 09:16:10 +0900

    Post 모델에 file_upload 필드 추가하기

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True) #77. head_image(게시물 맨 위의 이미지)를 넣을수 있도록 이렇게 입력한다.blank=True의 의미는 공백을 검사하지 않는다는 것이다. 즉 여기서는 이미지가 있을수도 없을수도 있게 하는것이다. 78. 이후 cmder에서 pip install Pillow(파이썬에서 이미지를 다룰수 있게 하는 패키지)로 설치하고 79. python manage.py makemigrations를 실행하면서 데이터베이스에 변화가 있다는 것을 알려주고 80. python manage.py migrate를 입력해서 데이터에 반영을 한다. 81. 이후 서버를 열어서 admin에서 이미지 파일 게시글에 추가해서 세이브 하면서 테스트 해 본다. 그러면 _media폴더 안에 년월일 폴더가 생기면서 이미지 파일이 저장이 된다. 이제 djshin_prj의 urls.py로 이동한다.
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True) #87. 파일 업로드를 위해 이렇게 입력한 후 cmder에서 python manage.py makemigrations를 한 후 python manage.py migrate를 해서 반영하고 admin 들어가서 파일을 저장해보며 테스트한다.
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
