21922d606878c8735533364e72c3bf7be381160a
dongjun <kyotostaily@naver.com>
2022-07-25 22:56:03 +0900

    포스트 목록 페이지와 포스트 상세 페이지 디자인 통일하기

--- blog/tests.py
+++ blog/tests.py
@@ -70,11 +70,11 @@ class TestView(TestCase): #113. 이런식으로 TestCase를 확장시켜준다.
         self.assertIn('About me', navbar.text) #143. navbar에 About me가 있는가 (지금은 없어서 에러난다. 향후 모듈화로 해결)
 
         # 2.3. 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어 있다.
-        self.assertIn(post_001.title, soup.title) #144. post_001의 타이틀이 soup에 있어야 한다는 뜻
+        self.assertIn(post_001.title, soup.title.text) #144. post_001의 타이틀이 soup에 있어야 한다는 뜻 161. 맨뒤에 .text를 추가한다(글자가 있는지 보기 위해서) 이제 post_detail.html의 8째줄로 이동한다.
         # 2.4. 첫 번째 포스트의 제목이 포스트 영역에 있다.
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area') #145. post_area는 main_area 안에 있고 id는 post-area다. 이렇게 안되있으면 html파일을 열어서 고쳐야된다.
         self.assertIn(post_001.title, post_area.text) #146. post_001의 title이 post_area.text 안에(self.assertIn) 있다는 뜻
         # 2.5. 첫 번째 포스트의 작성자(author)가 포스트 영역에 있다(아직 구현할 수 없음).
         # 2.6. 첫 번째 포스트의 내용(content)이 포스트 영역에 있다.
-        self.assertIn(post_001.content, post_area.text) #147. post_001의 content가 post_area.text 안에(self.assertIn) 있다는 뜻
+        self.assertIn(post_001.content, post_area.text) #147. post_001의 content가 post_area.text 안에(self.assertIn) 있다는 뜻, 148.post_list.html 파일을 복사해서 post_list.html이 있는 blog폴더에 붙여넣기를 하면서, base.html로 지정하고 들어간다.
