a8035acd60ba443b726ad316a0dcfdd03fde7e9b
dongjun <kyotostaily@naver.com>
2022-07-26 12:32:53 +0900

    포스트 목록, 상세 페이지에서 작성자 정보 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client #118. Client입력
+from django.contrib.auth.models import User #185. models.py에 가서 User의 사용을 위해 복사해서 붙여넣는다.
 from bs4 import BeautifulSoup #119.
 from .models import Post #120. models도 가져와야 하기에 임포트한다.
 
@@ -6,8 +7,16 @@ from .models import Post #120. models도 가져와야 하기에 임포트한다.
 class TestView(TestCase): #113. 이런식으로 TestCase를 확장시켜준다.
     def setUp(self): #116. setUp으로 시작한 후 (기본적으로 실행되는 함수이다.)
         self.client = Client() #117. 이렇게 입력하고 위(from)에 TestCase옆에 Client(방문하려는 사람의 브라우저)를 입력한다.
+        self.user_trump = User.objects.create_user( #184. 이와같이 입력하고
+            username='trump',
+            password='somepassword'
+        )
+        self.user_obama = User.objects.create_user( #186. 위의것처럼 하나 더 만들면 test_post_list에서 유저가 2명인 상태로 테스트를 시작한다. 이제 57줄로 간다.
+            username='obama',
+            password='somepassword'
+        )
 
-    def navbar_test(self, soup): #167.def navbar_test 의 내용을 아래와같이 입력한다. test라는 단어가 앞에 오면 TestCase에서 하나의 유닛단위로 보기 떄문에 navbar를 먼저 쓴다.
+    def navbar_test(self, soup): #167.def navbar_test 의 내용을 아래와같이 입력한다. test_라는 단어가 앞에 오면 TestCase에서 하나의 유닛단위로 보기 떄문에 navbar를 먼저 쓴다.
         navbar = soup.nav #168. 이 soup이 있어야 복사한 내용을 쓸 수 있다.
         self.assertIn('Blog', navbar.text) #170. Blog가 있는지 테스트
         self.assertIn('About me', navbar.text) #170. About me가 있는지 테스트, 171. 그리고 다시 37의 내용을 복사 해서 83 ~ 85의 내용을 지우고 그 자리에 붙여넣기 한다.
@@ -46,10 +55,12 @@ class TestView(TestCase): #113. 이런식으로 TestCase를 확장시켜준다.
         post_001 = Post.objects.create(
             title='첫 번째 포스트 입니다.',
             content='Hello, World. We are the World.',
+            author=self.user_trump #187. 작성자가 있어야 하는 상황이므로 위에서 입력했던 유저를 각각 입력한다.
         )
         post_002 = Post.objects.create(
             title='두 번째 포스트 입니다.',
             content='저는 쌀국수를 좋아합니다. ',
+            author=self.user_obama #187. 작성자가 있어야 하는 상황이므로 위에서 입력했던 유저를 각각 입력한다. 이제 77줄로 내려간다.
         )
         self.assertEqual(Post.objects.count(), 2) #130. 게시물을 카운트 해 봤을때 2개와 같은(self.assertEqual)지 확인한다.
 
@@ -63,11 +74,15 @@ class TestView(TestCase): #113. 이런식으로 TestCase를 확장시켜준다.
         # 3.4 "아직 게시물이 없습니다" 라는 문구가 없어야 한다.
         self.assertNotIn('아직 게시물이 없습니다', main_area.text) #136. 이 문구가 없어야(self.assertNotIn) 한다. 이제 test를 해 보고 확인한다.
 
+        self.assertIn(post_001.author.username.upper(), main_area.text) #188. post001의 author에 유저네임이 대문자(upper())로 있었으면 좋겠다는 뜻. 뒤에 main_area.text를 붙인다.
+        self.assertIn(post_002.author.username.upper(), main_area.text) #189. post002의 author에 유저네임이 대문자(upper())로 있었으면 좋겠다는 뜻. 뒤에 main_area.text를 붙인다. 이제 post_list.html의 27째 줄로 간다.
+
     def test_post_detail(self): #137. 위의 테스트 조건과는 다른 것을 한다.
         # 1.1. 포스트가 하나 있다. #139. 아래와 같이 포스트 1개를 만든다.
         post_001 = Post.objects.create(
             title='첫 번째 포스트 입니다.',
             content='Hello, World. We are the World.',
+            author=self.user_trump #191. post001에는 trump가 입력되 있으므로 이와같이 입력한다. 이제 107줄로 이동한다.
         )
         self.assertEqual(Post.objects.count(), 1) #138. 게시물을 카운트 해 봤을때 1개와 같은(self.assertEqual)지 확인한다.
         # 1.2. 그 포스트의 url은 ‘/blog/1/’ 이다.
@@ -89,5 +104,6 @@ class TestView(TestCase): #113. 이런식으로 TestCase를 확장시켜준다.
         post_area = main_area.find('div', id='post-area') #145. post_area는 main_area 안에 있고 id는 post-area다. 이렇게 안되있으면 html파일을 열어서 고쳐야된다.
         self.assertIn(post_001.title, post_area.text) #146. post_001의 title이 post_area.text 안에(self.assertIn) 있다는 뜻
         # 2.5. 첫 번째 포스트의 작성자(author)가 포스트 영역에 있다(아직 구현할 수 없음).
+        self.assertIn(self.user_trump.username.upper(), post_area.text) #192. 이 내용이 post_area안에 있으면 좋겠다는 뜻이다. 이제 post_detail.html의 18번째 줄로 간다.
         # 2.6. 첫 번째 포스트의 내용(content)이 포스트 영역에 있다.
         self.assertIn(post_001.content, post_area.text) #147. post_001의 content가 post_area.text 안에(self.assertIn) 있다는 뜻, 148.post_list.html 파일을 복사해서 post_list.html이 있는 blog폴더에 붙여넣기를 하면서, base.html로 지정하고 들어간다.
