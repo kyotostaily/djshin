d83071e07edd6a37417357d521a921daf0658d73
dongjun <kyotostaily@naver.com>
2022-07-26 11:44:27 +0900

    삭제된 작성자는 None으로 표시하기

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE) #181. 유저(작성자)가 탈퇴되면 글까지 같이 지워지는 기능
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) #181. 유저(작성자)가 탈퇴되면 글까지 같이 지워지는 기능 183.(User, on_delete=models.CASCADE) -> (User, null=True, on_delete=models.SET_NULL)로 바꾸고 makemigrations 후 migrate를 하면 작성자가 탈퇴해도 글은 남아있다.
 
     def __str__(self):
         return f'[{self.pk}] {self.title} :: {self.author}' #182. 이렇게 입력하고 makemigrations를 하고 선택지나오면(1번 후 1번)하고, migrate를 한다. 그러면 admin들어가보면 post란에 author(작성자) 선택란이 생겼다.
